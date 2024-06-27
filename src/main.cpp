37e425f9376145169d2119745872c39db9402df2
andang <dangthanhan507@gmail.com>
2024-06-27 00:30:51 -0400

    Added meshcat recording demo of allegro hand finalized. Weird placeholder at the end.

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@
 #include <drake/multibody/plant/multibody_plant_config_functions.h>
 #include <drake/multibody/parsing/process_model_directives.h>
 
-#include "drake/math/rigid_transform.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include <drake/systems/framework/diagram_builder.h>
 #include <drake/multibody/parsing/parser.h>
+#include <drake/visualization/visualization_config_functions.h>
+#include <drake/systems/framework/system_visitor.h>
+#include <drake/systems/framework/context.h>
 
 
-#define DT 0.001
+#define DT 0.01
 namespace drake {
 namespace multibody {
 
@@ -26,7 +28,7 @@ int run_main() {
     config.contact_surface_representation = "polygon";
     config.discrete_contact_approximation = "tamsi";
 
-    geometry::Meshcat meshcat;
+    std::shared_ptr<geometry::Meshcat> meshcat = std::make_shared<geometry::Meshcat>();
     systems::DiagramBuilder<double> builder;
 
     auto [plant, scene_graph] = AddMultibodyPlant(config, &builder);
@@ -34,8 +36,26 @@ int run_main() {
     Parser parser(&plant, &scene_graph);
     const parsing::ModelDirectives allegro_directive = parsing::LoadModelDirectives("../config/allegro_setup.yaml");
     parsing::ProcessModelDirectives(allegro_directive, &plant, nullptr, &parser);
+    plant.Finalize();
 
+    visualization::AddDefaultVisualization(&builder, meshcat);
 
+    std::unique_ptr<systems::Diagram<double>> diagram_ptr = builder.Build();
+    std::unique_ptr<systems::Context<double>> context = diagram_ptr->CreateDefaultContext();
+    auto& plant_context = plant.GetMyMutableContextFromRoot(context.get());
+    meshcat->StartRecording();
+    diagram_ptr->ForcedPublish(*context.get());
+    for (size_t i = 0 ; i < 100; ++i) {
+        context->SetTime(i * DT);
+        plant.SetPositions(&plant_context, Eigen::VectorXd::Zero(plant.num_positions()));
+        diagram_ptr->ForcedPublish(*context.get());
+    }
+    meshcat->StopRecording();
+    meshcat->PublishRecording();
+    diagram_ptr->ForcedPublish(*context.get());
+
+    int a;
+    std:: cin >> a;
     return 0;
 }
 
